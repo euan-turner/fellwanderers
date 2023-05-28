1384dd7c05d1112b0635417c3ea5a525d13cfba8
Euan Turner <euanoturner@gmail.com>
2023-05-28 21:40:35 +0100

    Fixed highlighting of active tab - implementation needs revision

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -1,5 +1,6 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useLocation } from "react-router-dom"
 import { Tab } from "@headlessui/react"
+import { useState } from "react"
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
@@ -12,27 +13,38 @@ export default function PageHeader() {
     { "id": 2, "link": "/committee", "text": "Committee"},
     { "id": 3, "link": "/archive", "text": "Archive"}
   ]
+  // Feels hacky - check with James
+  // Better to structure app with header, then page below?
+  // Or do this
+  // Or use redux to have state outside of component?
+  const location = useLocation();
+  const initialState = links.filter((link) => link.link === location.pathname)[0].id;
+  const [selectedIndex, setSelectedIndex] = useState(initialState);
   return (
     <div className={"flex w-screen justify-center px-2 py-4 sm:px-0"}>
-      <Tab.Group>
-        {/*TODO: Fix highlighting of tabs - highlighted when clicked off of link https://stackoverflow.com/questions/75176094/how-to-use-headless-ui-tabs-with-react-router-6*/}
+      <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
+        {/*TODO: First click does the routing, second click changes tab (once new page has been reached)
+        I believe this is because header is within each page, so the state 'resets' for each new page
+        */}
         <Tab.List className={"w-1/2 flex justify-evenly rounded-xl bg-blue-900/20 p-1"}>
           {links.map((link) => (
-            <Tab
-              key={link.id}
-              className={({ selected } ) =>
-                classNames(
-                  'w-1/4 rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700',
-                  'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
-                  selected
-                    ? 'bg-white shadow'
-                    : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
-                )
-              }
-            >
-              <NavLink to={link.link}>{link.text}</NavLink>
-            </Tab>
-          ))}
+            <NavLink to={link.link} className={"w-full"}>
+              <Tab
+                key={link.id}
+                className={({ selected } ) =>
+                  classNames(
+                    'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700',
+                    'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2',
+                    selected
+                      ? 'bg-white shadow'
+                      : 'text-blue-100 hover:bg-white/[0.12] hover:text-white'
+                  )
+                }
+              >{link.text}
+              </Tab>
+            </NavLink>
+          ))
+          }
         </Tab.List>
       </Tab.Group>
     </div>
