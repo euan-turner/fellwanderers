bfa1c36b166129923a5ad767bf7180869cedf77c
Euan Turner <euanoturner@gmail.com>
2023-07-07 17:23:01 +0100

    Improved Homepage layout with TextImage

--- src/components/TextImage.tsx
+++ src/components/TextImage.tsx
@@ -1,28 +1,28 @@
 interface TextImageProps {
   title: string;
-  text: string;
+  content: JSX.Element;
   src: string;
   alt: string;
   textLeft: boolean;
 }
 
 // TODO: Improve to take in a div for the text side, containing links and text: https://stackoverflow.com/questions/25797048/how-to-pass-in-a-react-component-into-another-react-component-to-transclude-the
-export default function TextImage({ title, text, src, alt, textLeft }: TextImageProps) {
-  const style = ["flex", textLeft ? "flex-row" : "flex-row-reverse", "h-60", "px-10", "py-5"];
+export default function TextImage({ title, content, src, alt, textLeft }: TextImageProps) {
+  const style = ["w-full", "h-full", "flex", textLeft ? "flex-row" : "flex-row-reverse", "rounded-md", "border-4", "border-green-900/20"];
   return (
-    
-    <div className={style.join(' ')}>
-      <div className={"w-1/2 p-4"}>
-        <h1 className={"text-3xl font-bold mb-2"}>
-          {title}
-        </h1>
-        <p className={"text-gray-700"}>
-          {text}
-        </p>
-      </div>
-      <div className={"w-1/2"}>
-        <img src={src} alt = {alt} className={"object-cover w-full h-full"}/>
+    <div className={"h-80 px-10 py-5"}>
+      <div className={style.join(' ')}>
+        <div className={"w-1/2 p-5"}>
+          <h1 className={"text-3xl font-bold mb-2"}>
+            {title}
+          </h1>
+          {content}
+        </div>
+        <div className={"w-1/2 p-5"}>
+          <img src={src} alt = {alt} className={"object-cover w-full h-full"}/>
+        </div>
       </div>
     </div>
+    
   )
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,31 +2,43 @@ import PageHeader from "../components/PageHeader";
 import TextImage from "../components/TextImage";
 
 export default function HomePage() {
+  const about: JSX.Element = (
+    <div>
+      <p>
+        We are Fellwanderers, the hiking society of Imperial College London.
+        From single-day getaways to weekend trips and holiday tours, we have you covered for all your hiking needs.
+      </p>
+      <p>
+        Love the hiking in the outdoors? Want to escape London for a bit?
+        Or even just want to chat and eat in beautiful places?
+        Then follow the link below to join us today!
+      </p>
+      <a href="https://www.imperialcollegeunion.org/activities/a-to-z/fellwanderers" target="_blank">Join Fellwanderers</a>
+    </div>
+  );
+  const shop: JSX.Element = (
+    <div>
+      <a href="https://www.imperialcollegeunion.org/shop/student-groups/407" target="_blank">Fellwanderers Shop</a>
+    </div>
+  );
+  const followUs: JSX.Element = (
+    <div>
+      <a href="https://www.instagram.com/icfellwanderers/" target="_blank">Follow Us</a>
+    </div>
+  );
+  const mail: JSX.Element = (
+    <div>
+      <a href="https://mailman.ic.ac.uk/mailman/listinfo/fellwanderers" target="_blank">Mailing List</a>
+    </div>
+  );
   return (
     <div>
       <PageHeader />
-      <TextImage title={"Join Us"} text={"Link to Website"} src={"templogo.png"} alt={"alt"} textLeft={true} />
-      <TextImage title={"Fellwanderers Shop"} text={"Link to Website"} src={"templogo.png"} alt={"alt"} textLeft={false} />
-      <TextImage title={"Mailing List"} text={"Link to Website"} src={"templogo.png"} alt={"alt"} textLeft={true} />
-      <TextImage title={"Follow Us"} text={"Link to Website"} src={"templogo.png"} alt={"alt"} textLeft={false} />
+      <TextImage title={"About"} content={about} src={"LakeDistrict22.jpg"} alt={"Lake District 2022"} textLeft={true} />
+      <TextImage title={"Fellwanderers Shop"} content={shop} src={"Snowdonia22.jpg"} alt={"Snowdonia 2022"} textLeft={false} />
+      <TextImage title={"Follow Us"} content={followUs} src={"Dales22.jpg"} alt={"Yorkshire Dales 2022"} textLeft={true} />
+      <TextImage title={"Mailing List"} content={mail} src={"JurassicCoast22.jpg"} alt={"Jurassic Coast 2022"} textLeft={false} />
       
-      <div>
-        <h3>Get Involved</h3>
-        <ul>
-          <li>
-            <a href="https://www.imperialcollegeunion.org/activities/a-to-z/fellwanderers" target="_blank">Join Us</a>
-          </li>
-          <li>
-            <a href="https://www.imperialcollegeunion.org/shop/student-groups/407" target="_blank">Fellwanderers Shop</a>
-          </li>
-          <li>
-            <a href="https://mailman.ic.ac.uk/mailman/listinfo/fellwanderers" target="_blank">Mailing List</a>
-          </li>
-          <li>
-            <a href="https://www.instagram.com/icfellwanderers/" target="_blank">Follow Us</a>
-          </li>
-        </ul>
-      </div>
       {/*nice formatting: https://flowbite.com/docs/typography/links/*/}
       {/*use heroicons*/}
     </div>
