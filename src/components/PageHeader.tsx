f91a91f06dc1fe8ebcae7b08a5e16b35e911ef26
Euan Turner <euanoturner@gmail.com>
2023-07-14 15:45:27 +0100

    Removed centered heading and restructured flex boxes

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -15,39 +15,34 @@ export default function PageHeader() {
   const location = useLocation();
   const page = links.filter((link) => link.link === location.pathname)[0].id;
   return (
-    <div className={"flex w-screen justify-around items-center h-30 px-2 py-4 sm:px-0"}>
-      <div className={"w-1/4"}>
-        <NavLink to={"/"}>
-          <img className={"px-5 h-24 object-center"} src={"logo.png"} alt={"society logo"} />
-        </NavLink>
+    <div className={"flex justify-center items-center w-screen h-30 px-2 py-4 sm:px-0"}>
+      <NavLink to={"/"} className={"w-1/2"}>
+        <img className={"px-5 h-24 mx-auto"} src={"logo.png"} alt={"society logo"} />
+      </NavLink>
+      <div className={"w-1/2"}>
+        <Tab.Group selectedIndex={page}>
+          <Tab.List className={"max-h-12 w-1/2 mx-auto flex justify-center items-center rounded-xl bg-green-900/20 p-1"}>
+            {links.map((link) => (
+              <NavLink to={link.link} className={"w-full"}>
+                <Tab
+                  key={link.id}
+                  className={({ selected } ) =>
+                    classNames(
+                      'w-full rounded-md px-2.5 py-2.5 text-sm font-medium leading-5 text-green-700',
+                      'ring-white ring-opacity-60 ring-offset-2 ring-offset-green-600 focus:outline-none focus:ring-2',
+                      selected
+                        ? 'bg-white shadow'
+                        : 'text-green-600 hover:bg-white/[0.12] hover:text-green-800'
+                    )
+                  }
+                >{link.text}
+                </Tab>
+              </NavLink>
+            ))
+            }
+          </Tab.List>
+        </Tab.Group>
       </div>
-      <div className={"w-1/4"}>
-        <h1 className={"mb-4 text-2xl text-center font-extrabold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl"}>
-          Fellwanderers
-        </h1>
-      </div>
-      <Tab.Group selectedIndex={page} >
-        <Tab.List className={"max-h-12 w-1/4 flex justify-center items-center rounded-xl bg-green-900/20 p-1"}>
-          {links.map((link) => (
-            <NavLink to={link.link} className={"w-full"}>
-              <Tab
-                key={link.id}
-                className={({ selected } ) =>
-                  classNames(
-                    'w-full rounded-md px-2.5 py-2.5 text-sm font-medium leading-5 text-green-700',
-                    'ring-white ring-opacity-60 ring-offset-2 ring-offset-green-600 focus:outline-none focus:ring-2',
-                    selected
-                      ? 'bg-white shadow'
-                      : 'text-green-600 hover:bg-white/[0.12] hover:text-green-800'
-                  )
-                }
-              >{link.text}
-              </Tab>
-            </NavLink>
-          ))
-          }
-        </Tab.List>
-      </Tab.Group>
     </div>
   )
 }
