7b996cf801cfad3b6ec22468f3e12c63fee05f91
Euan Turner <euanoturner@gmail.com>
2023-07-14 20:55:58 +0100

    Initial hero section for homepage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,11 @@
 import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faPersonHiking, faBagShopping, faHashtag, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faPersonHiking, faBagShopping, faHashtag, faEnvelope, faArrowRight, faCircleInfo } from "@fortawesome/free-solid-svg-icons";
 
 import PageHeader from "../components/PageHeader";
 import TextImage from "../components/TextImage";
 import StyledLink from "../components/StyledLink.tsx";
 
-// const socLink = "https://www.imperialcollegeunion.org/activities/a-to-z/fellwanderers";
+const socLink = "https://www.imperialcollegeunion.org/activities/a-to-z/fellwanderers";
 const joinLink = "https://www.imperialcollegeunion.org/shop/csp/fellwanderers/fellwanderers-imperial-hiking-society-membership-22-23";
 const shopLink = "https://www.imperialcollegeunion.org/shop/student-groups/407";
 const instaLink = "https://www.instagram.com/icfellwanderers";
@@ -120,10 +120,45 @@ const sections = [
     textLeft: false
   }
 ]
+
+function HeroSection() {
+  const backgroundImageStyle = {
+    backgroundImage: `url(/hike_pics/LakeDistrict22.jpg)`
+  }
+  return (
+    <section className={"bg-cover bg-center h-screen flex flex-col justify-start items-start p-16"} style={backgroundImageStyle}>
+      <div className={"w-1/2"}>
+        <h1 className={"text-6xl font-bold px-10"}>Fellwanderers</h1>
+        <h2 className={"text-4xl px-10 pb-5"}>Imperial's Hiking Society</h2>
+        <div className={"flex flex-row w-1/2 justify-start space-x-5 px-10"}>
+          <StyledLink
+            href={joinLink}
+            className={"inline-block p-2 bg-green-900/50 font-semibold rounded-md no-underline hover:bg-green-900/60"}
+            children={(
+              <div>
+                <FontAwesomeIcon icon={faPersonHiking} /> Join Now <FontAwesomeIcon icon={faArrowRight} />
+              </div>
+            )}
+          />
+          <StyledLink
+            href={socLink}
+            className={"inline-block p-2 bg-green-900/50 font-semibold rounded-md no-underline hover:bg-green-900/60"}
+            children={(
+              <div>
+                <FontAwesomeIcon icon={faCircleInfo} /> More Info <FontAwesomeIcon icon={faArrowRight} />
+              </div>
+            )}
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
 export default function HomePage() {
   return (
     <div>
       <PageHeader />
+      <HeroSection />
       <div className={"grid grid-cols-1"}>
         {sections.map((section) => (
           <TextImage title={section.title} content={section.content} src={section.src} alt={section.alt} textLeft={section.textLeft} />
