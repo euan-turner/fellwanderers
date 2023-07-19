d748c5be59d201012a06500f2c1dd30da564e877
Euan Turner <euanoturner@gmail.com>
2023-07-19 15:13:23 +0100

    Temporary storage for activity data

--- src/pages/ActivitiesPage.tsx
+++ src/pages/ActivitiesPage.tsx
@@ -1,5 +1,9 @@
+import { doc, setDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
 import PageHeader from "../components/PageHeader";
 import PageFooter from "../components/PageFooter";
+import { db } from "../../firebase.ts";
 
 enum Type {
   Hike,
@@ -7,9 +11,8 @@ enum Type {
 }
 
 interface Activity {
-  id: number;
   title: string;
-  date: string;
+  date: Date;
   type: Type;
   misc: string;
 }
@@ -19,16 +22,16 @@ interface CalendarProps {
 }
 
 const activities = [
-  { id: 1, title: 'Afternoon Tea @ SK', date: '26-05-2023', type: Type.Social, misc: 'Meet on Queen\'s Lawn' },
-  { id: 2, title: 'Afternoon Tea @ SK', date: '02-06-2023', type: Type.Social, misc: 'Meet on Queen\'s Lawn' },
-  { id: 3, title: 'Afternoon Tea @ SK', date: '09-05-2023', type: Type.Social, misc: 'Meet on Queen\'s Lawn' },
-  { id: 4, title: 'Afternoon Tea @ SK', date: '16-05-2023', type: Type.Social, misc: 'Meet on Queen\'s Lawn' },
-  { id: 5, title: 'Union Bar Night', date: '01-05-2023', type: Type.Social, misc: 'Union Quiz Night' },
-  { id: 6, title: 'Picnic', date: '22-05-2023', type: Type.Social, misc: 'Location TBC' },
-  { id: 7, title: 'High Wycombe to Oxford', date: '27-05-2023', type: Type.Hike, misc: '50km' },
-  { id: 8, title: 'Epping Forest', date: '03-06-2023', type: Type.Hike, misc: 'Mindfulness Society Collab' },
-  { id: 9, title: 'Lewes', date: '10-06-2023', type: Type.Hike, misc: 'Shorter Day Hike' },
-  { id: 10, title: 'Chiltern Hills', date: '25-06-2023', type: Type.Hike, misc: 'Shorter Day Hike'},
+  { title: 'Afternoon Tea @ SK', date: new Date(2023, 5, 26), type: Type.Social, misc: 'Meet on Queen\'s Lawn' },
+  { title: 'Afternoon Tea @ SK', date: new Date(2023, 6, 2), type: Type.Social, misc: 'Meet on Queen\'s Lawn' },
+  { title: 'Afternoon Tea @ SK', date: new Date(2023, 6, 9), type: Type.Social, misc: 'Meet on Queen\'s Lawn' },
+  { title: 'Afternoon Tea @ SK', date: new Date(2023, 6, 16), type: Type.Social, misc: 'Meet on Queen\'s Lawn' },
+  { title: 'Union Bar Night', date: new Date(2023, 5, 1), type: Type.Social, misc: 'Union Quiz Night' },
+  { title: 'Picnic', date: new Date(2023, 5, 25), type: Type.Social, misc: 'Location TBC' },
+  { title: 'High Wycombe to Oxford', date: new Date(2023, 5, 27), type: Type.Hike, misc: '50km' },
+  { title: 'Epping Forest', date: new Date(2023, 6, 3), type: Type.Hike, misc: 'Mindfulness Society Collab' },
+  { title: 'Lewes', date: new Date(2023, 6, 10), type: Type.Hike, misc: 'Shorter Day Hike' },
+  { title: 'Chiltern Hills', date: new Date(2023, 6, 25), type: Type.Hike, misc: 'Shorter Day Hike'},
 ];
 
 function colourActivity(type: Type): string {
@@ -46,9 +49,9 @@ function Calendar({ activities }: CalendarProps) {
       <h1 className={"text-2xl font-bold mb-4"}>Upcoming Activities</h1>
       <div className={"grid grid-cols-3 gap-4"}>
         {activities.map((activity) => (
-          <div key={activity.id} className={colourActivity(activity.type).concat(" shadow-md p-4")}>
+          <div key={activity.date.toString()} className={colourActivity(activity.type).concat(" shadow-md p-4")}>
             <h2 className={"text-lg font-semibold"}>{activity.title}</h2>
-            <h3 className={"text-gray-700"}>{activity.date}</h3>
+            <h3 className={"text-gray-700"}>{activity.date.toString()}</h3>
             <p className={"text-gray-500"}>{activity.misc}</p>
           </div>
         ))}
@@ -57,12 +60,40 @@ function Calendar({ activities }: CalendarProps) {
   );
 }
 
+async function setActivitiesData() {
+  for (const activity of activities) {
+    await setDoc(doc(db, "activities", String(activity.date)), {
+      title: activity.title,
+      date: activity.date,
+      misc: activity.misc,
+      type: activity.type,
+    });
+  }
+}
+
 export default function ActivitiesPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  
+  useEffect(() => {
+    setActivitiesData()
+      .then(() => {
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error setting activities data:", error);
+      });
+  }, []);
   return (
     <>
       <PageHeader />
-      <Calendar activities={activities} />
-      <PageFooter />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <Calendar activities={activities} />
+          <PageFooter />
+        </>
+      )}
     </>
   )
 }
