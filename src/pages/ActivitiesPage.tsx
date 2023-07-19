efaf5d53306586db730f4a5402fc75316488deb0
Euan Turner <euanoturner@gmail.com>
2023-07-19 15:52:45 +0100

    Retrieving activity data from firestore

--- src/pages/ActivitiesPage.tsx
+++ src/pages/ActivitiesPage.tsx
@@ -1,4 +1,4 @@
-import { doc, setDoc } from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 import PageHeader from "../components/PageHeader";
@@ -21,19 +21,6 @@ interface CalendarProps {
   activities: Activity[];
 }
 
-const activities = [
-  { title: 'Afternoon Tea @ SK', date: new Date(2023, 5, 26), type: Type.Social, misc: 'Meet on Queen\'s Lawn' },
-  { title: 'Afternoon Tea @ SK', date: new Date(2023, 6, 2), type: Type.Social, misc: 'Meet on Queen\'s Lawn' },
-  { title: 'Afternoon Tea @ SK', date: new Date(2023, 6, 9), type: Type.Social, misc: 'Meet on Queen\'s Lawn' },
-  { title: 'Afternoon Tea @ SK', date: new Date(2023, 6, 16), type: Type.Social, misc: 'Meet on Queen\'s Lawn' },
-  { title: 'Union Bar Night', date: new Date(2023, 5, 1), type: Type.Social, misc: 'Union Quiz Night' },
-  { title: 'Picnic', date: new Date(2023, 5, 25), type: Type.Social, misc: 'Location TBC' },
-  { title: 'High Wycombe to Oxford', date: new Date(2023, 5, 27), type: Type.Hike, misc: '50km' },
-  { title: 'Epping Forest', date: new Date(2023, 6, 3), type: Type.Hike, misc: 'Mindfulness Society Collab' },
-  { title: 'Lewes', date: new Date(2023, 6, 10), type: Type.Hike, misc: 'Shorter Day Hike' },
-  { title: 'Chiltern Hills', date: new Date(2023, 6, 25), type: Type.Hike, misc: 'Shorter Day Hike'},
-];
-
 function colourActivity(type: Type): string {
   switch (type) {
     case Type.Hike:
@@ -51,7 +38,7 @@ function Calendar({ activities }: CalendarProps) {
         {activities.map((activity) => (
           <div key={activity.date.toString()} className={colourActivity(activity.type).concat(" shadow-md p-4")}>
             <h2 className={"text-lg font-semibold"}>{activity.title}</h2>
-            <h3 className={"text-gray-700"}>{activity.date.toString()}</h3>
+            <h3 className={"text-gray-700"}>{activity.date.toDateString()}</h3>
             <p className={"text-gray-500"}>{activity.misc}</p>
           </div>
         ))}
@@ -60,40 +47,34 @@ function Calendar({ activities }: CalendarProps) {
   );
 }
 
-async function setActivitiesData() {
-  for (const activity of activities) {
-    await setDoc(doc(db, "activities", String(activity.date)), {
-      title: activity.title,
-      date: activity.date,
-      misc: activity.misc,
-      type: activity.type,
-    });
-  }
+async function retrieveActivitiesData() {
+  const querySnapshot = await getDocs(collection(db, "activities"));
+  const activities: Activity[] = [];
+  querySnapshot.forEach((activity) => {
+    const act = activity.data();
+    act.date = act.date.toDate();
+    activities.push(act as Activity)
+  })
+  return activities;
 }
 
 export default function ActivitiesPage() {
-  const [isLoading, setIsLoading] = useState(true);
+  const [activityData, setActivityData] = useState<Activity[]>([]);
   
   useEffect(() => {
-    setActivitiesData()
-      .then(() => {
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error("Error setting activities data:", error);
-      });
-  }, []);
+    retrieveActivitiesData()
+      .then((activities) => {
+      setActivityData(activities);
+    })
+    .catch((error) => {
+      console.error(error);
+    })
+  }, [])
   return (
     <>
       <PageHeader />
-      {isLoading ? (
-        <p>Loading...</p>
-      ) : (
-        <>
-          <Calendar activities={activities} />
-          <PageFooter />
-        </>
-      )}
+      <Calendar activities={activityData} />
+      <PageFooter />
     </>
   )
 }
