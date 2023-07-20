e7b14eb5d403e17c4babd6f4688588ebed4dc419
Euan Turner <euanoturner@gmail.com>
2023-07-20 20:49:08 +0100

    Upcoming activities grouped by week

--- src/pages/ActivitiesPage.tsx
+++ src/pages/ActivitiesPage.tsx
@@ -30,16 +30,48 @@ function colourActivity(type: Type): string {
   }
 }
 
+const isSameWeek = (date1: Date, date2: Date) => {
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneWeek = oneDay * 7;
+  const diff = Math.abs(date1.getTime() - date2.getTime());
+  if (diff >= oneWeek) return false; // More than 7 days apart
+  let day1 = date1.getDay();
+  let day2 = date2.getDay();
+  if (day1 === 0) day1 = 7;
+  if (day2 === 0) day2 = 7;
+  return Math.ceil((date1.getTime() - (day1 - 1) * oneDay) / oneWeek)
+    === Math.ceil((date2.getTime() - (day2 - 1) * oneDay) / oneWeek);
+}
+
 function Calendar({ activities }: CalendarProps) {
+  const weeks: Activity[][] = [];
+  let currentWeek: Activity[] = [];
+  activities.forEach((activity) => {
+    if (currentWeek.length === 0 || isSameWeek(currentWeek[currentWeek.length - 1].date, activity.date)
+    ) {
+      currentWeek.push(activity);
+    } else {
+      weeks.push(currentWeek);
+      currentWeek = [activity];
+    }
+  })
+  if (currentWeek.length > 0) {
+    weeks.push(currentWeek);
+  }
+  console.log(weeks);
   return (
     <div className={"container mx-auto py-8"}>
       <h1 className={"text-2xl font-bold mb-4"}>Upcoming Activities</h1>
-      <div className={"grid grid-cols-3 gap-4"}>
-        {activities.map((activity) => (
-          <div key={activity.date.toString()} className={colourActivity(activity.type).concat(" shadow-md p-4")}>
-            <h2 className={"text-lg font-semibold"}>{activity.title}</h2>
-            <h3 className={"text-gray-700"}>{activity.date.toDateString()}</h3>
-            <p className={"text-gray-500"}>{activity.misc}</p>
+      <div className={"flex flex-col"}>
+        {weeks.map((week, weekIndex) => (
+          <div key={weekIndex} className={"flex flex-row gap-4"}>
+            {week.map((act, actIndex) => (
+              <div key={actIndex} className={colourActivity(act.type).concat(" shadow-md p-4")}>
+                <h2 className={"text-lg font-semibold"}>{act.title}</h2>
+                <h3 className={"text-gray-700"}>{act.date.toDateString()}</h3>
+                <p className={"text-gray-500"}>{act.misc}</p>
+              </div>
+            ))}
           </div>
         ))}
       </div>
