581bef7e73dcb722ca7d509b8af112a41be514fa
Euan Turner <euanoturner@gmail.com>
2023-07-24 15:33:18 +0100

    Archive page uses firestore

--- src/pages/ArchivePage.tsx
+++ src/pages/ArchivePage.tsx
@@ -1,36 +1,58 @@
-import { LoremIpsum } from "lorem-ipsum";
+import {collection, getDocs} from "firebase/firestore";
+import {useEffect, useState} from "react";
 
 import HikeArchive from "../components/HikeArchive";
 import PageHeader from "../components/PageHeader";
 import PageFooter from "../components/PageFooter";
+import {db} from "../../firebase.ts";
 
-const lorem = new LoremIpsum({
-  sentencesPerParagraph: {
-    max: 8,
-    min: 4
-  },
-  wordsPerSentence: {
-    max: 16,
-    min: 4
-  }
-});
+interface Hike {
+  title: string;
+  desc: string;
+  directory: string;
+  textLeft: boolean;
+}
 
-const hikes = [
-  {
-    title: "Stonehenge 2022",
-    desc: lorem.generateParagraphs(6),
-    directory: "images/archive/stonehenge22",
-    textLeft: true,
-  },
-  {
-    title: "Yorkshire 2022",
-    desc: lorem.generateParagraphs(6),
-    directory: "images/archive/yorkshire22",
-    textLeft: false,
-  }
-]
+async function retrieveArchiveData() {
+  const querySnapshot = await getDocs(collection(db, "archive"));
+  const hikes: Hike[] = [];
+  querySnapshot.forEach((hike) => {
+    hikes.push(hike.data() as Hike);
+  })
+  return hikes;
+}
 
 export default function ArchivePage() {
+  const [hikeData, setHikeData] = useState<Hike[]>([]);
+  
+  useEffect(() => {
+    const getCachedHikes = () => {
+      const cachedHikes = localStorage.getItem("archive");
+      if (cachedHikes) {
+        return JSON.parse(cachedHikes);
+      }
+      return null;
+    }
+    const fetchHikesAndCache = async () => {
+      retrieveArchiveData()
+        .then((hikes) => {
+          setHikeData(hikes);
+          localStorage.setItem("archive", JSON.stringify(hikes));
+        })
+      .catch((error) => {
+        console.error(error);
+      })
+    }
+    const cachedHikes = getCachedHikes();
+    if (cachedHikes) {
+      setHikeData(cachedHikes);
+    } else {
+      fetchHikesAndCache()
+        .catch((error) => {
+          console.error("Error fetching archive: ", error);
+        });
+    }
+  }, [])
   return (
     <>
       <PageHeader />
@@ -43,7 +65,7 @@ export default function ArchivePage() {
             Looking for some hiking inspiration? Here's a selection of some of the hikes we've been on in the past
           </p>
         </div>
-        {hikes.map((hike) => (
+        {hikeData.map((hike) => (
           <div key={hike.title}>
             <HikeArchive title={hike.title} desc={hike.desc} directory={hike.directory} textLeft={hike.textLeft} />
           </div>
