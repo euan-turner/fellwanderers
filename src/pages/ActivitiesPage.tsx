c78f64028c9a88111127c271de3df03c34765f0f
Euan Turner <euanoturner@gmail.com>
2023-07-24 17:49:29 +0100

    Extracted calendar component

--- src/pages/ActivitiesPage.tsx
+++ src/pages/ActivitiesPage.tsx
@@ -3,9 +3,8 @@ import { useEffect, useState } from "react";
 
 import PageHeader from "../components/PageHeader";
 import PageFooter from "../components/PageFooter";
+import Calendar from "../components/Calendar";
 import { db } from "../../firebase.ts";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faChevronLeft, faChevronRight} from "@fortawesome/free-solid-svg-icons";
 
 enum Type {
   Hike,
@@ -18,116 +17,6 @@ interface Activity {
   date: Date;
   type: Type;
   misc: string;
-}
-
-interface CalendarProps {
-  activities: Activity[];
-}
-
-function colourActivity(type: Type): string {
-  switch (type) {
-    case Type.Blank:
-      return 'bg-white';
-    case Type.Hike:
-      return 'bg-green-200';
-    case Type.Social:
-      return 'bg-orange-200';
-  }
-}
-
-const firstMondayOfMonth = (date: Date) => {
-  const tempDate = new Date(date);
-  const day = date.getDay();
-  const diff = date.getDate() - day + (day === 0 ? -6 : 1);
-  tempDate.setDate(diff);
-  return tempDate;
-}
-
-const lastSundayOfMonth = (date: Date) => {
-  const tempDate = new Date(date);
-  tempDate.setDate(1);
-  tempDate.setMonth(date.getMonth() + 1);
-  while (tempDate.getDay() !== 0) {
-    tempDate.setDate(tempDate.getDate() + 1);
-  }
-  return tempDate;
-}
-
-// PRE: planned activities are in chronological order
-const createMonthActivities = (startDate: Date, planned: Activity[]) => {
-  const currDate = firstMondayOfMonth(startDate);
-  const endDate = lastSundayOfMonth(startDate);
-  const monthActivities = planned.filter((activity) => {
-    const actDate = activity.date;
-    return actDate >= currDate && actDate <= endDate;
-  });
-  const activities: Activity[] = [];
-  while (currDate.getTime() <= endDate.getTime()) {
-    if ((monthActivities.length > 0) && (currDate.toString() === monthActivities[0].date.toString())) {
-      activities.push(monthActivities.shift() as Activity);
-    } else {
-      activities.push({
-        title: "",
-        date: new Date(currDate),
-        type: Type.Blank,
-        misc: "",
-      });
-    }
-    currDate.setDate(currDate.getDate() + 1);
-  }
-  return activities;
-}
-
-function Calendar({ activities }: CalendarProps) {
-  const [monthStart, setMonthStart] = useState<Date>(new Date(2023, 5, 1));
-  const [prevDisabled, setPrevDisabled] = useState(false);
-  const [nextDisabled, setNextDisabled] = useState(false);
-  const earliest = new Date(2023, 4, 1);
-  const latest = new Date(2023, 7, 1);
-  const nextMonth = () => {
-    setMonthStart(new Date(monthStart.setMonth(monthStart.getMonth() + 1)));
-    setNextDisabled(monthStart.getTime() >= latest.getTime());
-    setPrevDisabled(false);
-  }
-  const prevMonth = () => {
-    setMonthStart(new Date(monthStart.setMonth(monthStart.getMonth() - 1)));
-    setPrevDisabled(monthStart.getTime() <= earliest.getTime());
-    setNextDisabled(false);
-  }
-  const dateFormat: Intl.DateTimeFormatOptions = { year: 'numeric', month: 'long'};
-  const monthActivities = createMonthActivities(monthStart, activities);
-  return (
-    <div className={"container mx-auto py-8"}>
-      <div className={"flex justify-center items-center mb-4"}>
-        <button className={"bg-white hover:scale-y-110 py-2 px-4 rounded-lg"} onClick={prevMonth} disabled={prevDisabled}>
-          {!prevDisabled && (
-            <FontAwesomeIcon icon={faChevronLeft} />
-          )}
-        </button>
-        <h1 className={"text-2xl font-bold"}>
-          {monthStart.toLocaleDateString('default', dateFormat)}
-        </h1>
-        
-        <button className={"bg-white hover:scale-y-110 font-bold py-2 px-4 rounded-lg"} onClick={nextMonth} disabled={nextDisabled}>
-          {!nextDisabled && (
-            <FontAwesomeIcon icon={faChevronRight} />
-          )}
-        </button>
-        
-      </div>
-
-      <div className={"grid grid-rows-5 grid-cols-7 gap-4 max-h-screen"}>
-        {monthActivities.map((act, actIndex) => (
-          <div key={actIndex} className={colourActivity(act.type).concat(" shadow-md p-4 h-32")}>
-            <h3 className={"text-gray-700"}>{act.date.toDateString()}</h3>
-            <h2 className={"text-lg font-semibold"}>{act.title}</h2>
-            <p className={"text-gray-500"}>{act.misc}</p>
-          </div>
-          )
-        )}
-      </div>
-    </div>
-  );
 }
 
 async function retrieveActivitiesData() {
