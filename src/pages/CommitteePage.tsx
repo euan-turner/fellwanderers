d70861c6c1321d1ff277412856a353fcd253bdb0
Euan Turner <euanoturner@gmail.com>
2023-07-25 17:43:14 +0100

    CommitteePage styled for mobile

--- src/pages/CommitteePage.tsx
+++ src/pages/CommitteePage.tsx
@@ -61,15 +61,15 @@ export default function CommitteePage() {
   return (
     <>
       <PageHeader />
-      <div className="bg-white py-24 sm:py-32">
-        <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-20 px-6 lg:px-8 xl:grid-cols-3">
+      <div className="bg-white py-8 lg:py-24">
+        <div className="mx-auto grid max-w-7xl gap-x-8 gap-y-8 px-6 lg:px-8 lg:grid-cols-3">
           <div className="max-w-2xl">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Meet your committee</h2>
-            <p className="mt-6 text-lg leading-8 text-gray-600">
+            <p className="mt-6 sm:text-lg leading-8 text-gray-600">
               {lorem.generateParagraphs(1)}
             </p>
           </div>
-          <ul role="list" className="grid gap-x-12 gap-y-12 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2">
+          <ul role="list" className="grid gap-x-12 gap-y-8 sm:grid-cols-2 sm:gap-y-16 xl:col-span-2 ml-4">
             {people.map((person) => (
               <li key={person.name}>
                 <div className="overflow-visible relative max-w-sm mx-auto bg-logoGreen-light border border-logoGreen-dark shadow-lg ring-1 ring-black/5 rounded-xl flex items-center gap-6">
