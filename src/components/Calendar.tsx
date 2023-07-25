d05130baa344a9b9682d0f9b98129e92b5c7b763
Euan Turner <euanoturner@gmail.com>
2023-07-25 17:37:25 +0100

    Calendar styled for mobile

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -99,18 +99,19 @@ export default function Calendar({ activities }: CalendarProps) {
     setPrevDisabled(monthStart.getTime() <= earliest.getTime());
     setNextDisabled(false);
   };
-  const dateFormat: Intl.DateTimeFormatOptions = {
+  const titleDateFormat = new Intl.DateTimeFormat("en-GB", {
     year: "numeric",
     month: "long",
-  };
+  });
+  const tileDateFormat = new Intl.DateTimeFormat("en-GB", { weekday: "short", day: "numeric" })
 
   useEffect(() => {
     setMonthActivities(createMonthActivities(monthStart, activities));
   }, [monthStart, activities]);
 
   return (
-    <div className={"container mx-auto py-8"}>
-      <div className={"flex justify-center items-center mb-4"}>
+    <div className={"container mx-auto lg:py-8"}>
+      <div className={"flex justify-center items-center lg:mb-4"}>
         <button
           className={"bg-white hover:scale-y-110 py-2 px-4 rounded-lg"}
           onClick={prevMonth}
@@ -118,8 +119,8 @@ export default function Calendar({ activities }: CalendarProps) {
         >
           {!prevDisabled && <FontAwesomeIcon icon={faChevronLeft} />}
         </button>
-        <h1 className={"text-2xl font-bold"}>
-          {monthStart.toLocaleDateString("default", dateFormat)}
+        <h1 className={"text-xl xl:text-2xl font-bold"}>
+          {titleDateFormat.format(monthStart)}
         </h1>
 
         <button
@@ -132,19 +133,20 @@ export default function Calendar({ activities }: CalendarProps) {
           {!nextDisabled && <FontAwesomeIcon icon={faChevronRight} />}
         </button>
       </div>
-
-      <div className={"flex flex-col h-[725px] overflow-y-scroll"}>
-        <div className={"grid grid-rows-5 grid-cols-7 gap-4 p-2"}>
-          {monthActivities.map((act, actIndex) => (
-            <div
-              key={actIndex}
-              className={colourActivity(act.type).concat(" shadow-md p-4 h-32 border border-slate-400")}
-            >
-              <h3 className={"text-gray-700"}>{act.date.toDateString()}</h3>
-              <h2 className={"text-lg font-semibold"}>{act.title}</h2>
-              <p className={"text-gray-500"}>{act.misc}</p>
-            </div>
-          ))}
+      <div className={"w-full overflow-x-scroll"}>
+        <div className={"inline-flex flex-col w-[800px] min-h-max lg:w-screen lg:h-[725px] overflow-y-scroll"}>
+          <div className={"grid grid-rows-5 grid-cols-7 gap-y-1 gap-x-0.5 lg:gap-4 p-2"}>
+            {monthActivities.map((act, actIndex) => (
+              <div
+                key={actIndex}
+                className={colourActivity(act.type).concat(" shadow-md p-1 lg:p-4 h-32 border border-slate-400")}
+              >
+                <h3 className={"text-sm lg:text-base text-gray-700"}>{tileDateFormat.format(act.date)}</h3>
+                <h2 className={"text-base lg:text-lg font-semibold"}>{act.title}</h2>
+                <p className={"text-sm lg:text-base text-gray-500"}>{act.misc}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
