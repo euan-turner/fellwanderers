0ac3d74aaeb1541a5d145ea8cf6709045510a59f
Euan Turner <euanoturner@gmail.com>
2023-07-27 17:42:08 +0100

    Aligned FAQ styles

--- src/pages/FaqPage.tsx
+++ src/pages/FaqPage.tsx
@@ -74,12 +74,12 @@ const FAQ = ({question, answer}: faq) => {
     <Disclosure>
       {({ open }) => (
         <>
-          <Disclosure.Button className="flex w-full justify-between rounded-lg bg-purple-100 px-4 py-2 text-left text-sm font-medium text-purple-900 hover:bg-purple-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+          <Disclosure.Button className="flex w-full justify-between rounded-lg bg-logoGreen-light px-4 py-2 text-left text-sm font-medium text-logoGreen-dark border border-logoGreen-dark hover:bg-logoGreen-light/70 focus:outline-none focus-visible:ring focus-visible:ring-green-500 focus-visible:ring-opacity-75">
             <span>{question}</span>
             <FontAwesomeIcon icon={faChevronDown}
               className={`${
                 open ? 'rotate-180 transform' : ''
-              } h-5 w-5 text-purple-500`}
+              } h-5 w-5 text-logoGreen-dark`}
             />
           </Disclosure.Button>
           <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm text-gray-500">
