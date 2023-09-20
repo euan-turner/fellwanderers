6c847652ebabe62dba1e0448a7ac81dec673b288
Euan Turner <euanoturner@gmail.com>
2023-09-20 11:37:48 +0100

    Better formatting for home page about

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -21,7 +21,7 @@ function HeroSection() {
   return (
     <section
       className={
-        "bg-cover h-screen flex flex-col justify-start items-start py-16"
+        "bg-cover h-screen flex flex-col justify-start items-start py-8"
       }
       style={backgroundImageStyle}
     >
@@ -62,20 +62,30 @@ function HeroSection() {
 function Description() {
   return (
     <div className="absolute inset-0 flex items-center justify-center">
-      <div className="bg-white bg-opacity-50 p-8 rounded-lg w-3/4">
-        <p className="mt-4 text-lg text-gray-600 text-center">
-          About Us:
-          As the official hiking club for Imperial College London, we are your gateway to discovering the breathtaking landscapes and hidden gems of the United Kingdom. We specialize in crafting unforgettable hiking experiences that encompass day hikes, thrilling weekend trips, and captivating tours across the UK.
-          
-          Explore Nature, Create Memories:
+      <div className="bg-white bg-opacity-50 p-4 rounded-lg w-3/4">
+        <h1 className="text-xl font-semibold text-center text-gray-800">
+          About Us
+        </h1>
+        <p className="my-2 text-lg text-gray-600 text-center">
+          As the official hiking society for Imperial College London, we are your gateway to discovering the breathtaking landscapes and hidden gems of the United Kingdom. We will take you on unforgettable hiking experiences that encompass day hikes, thrilling weekend trips, and captivating tours across the UK.
+        </p>
+        <h1 className="text-xl font-semibold text-center text-gray-800">
+          Explore Nature, Create Memories
+        </h1>
+        <p className="my-2 text-lg text-gray-600 text-center">
           But we're more than just a hiking club; we're a community of like-minded individuals who share a passion for the great outdoors. When you join us, you're not just signing up for hikes; you're joining a group of kindred spirits who relish the opportunity to escape the bustling city of London and immerse themselves in the tranquility of nature. Our hikes aren't just about the breathtaking views and challenging trails; they're about the connections you make and the conversations you have along the way.
-          
-          Hiking and So Much More:
+        </p>
+        <h1 className="text-xl font-semibold text-center text-gray-800">
+          Hiking and So Much More
+        </h1>
+        <p className="my-2 text-lg text-gray-600 text-center">
           From striking up great conversations while trekking through beautiful landscapes to unforgettable social events both on campus and in the heart of London, our club offers a vibrant social aspect that complements our outdoor adventures. Whether it's a casual chat during a hike or a lively gathering at one of our socials, you'll find yourself surrounded by welcoming faces and fantastic company.
-          
-          Join the Adventure:
+        </p>
+        <h1 className="text-xl font-semibold text-center text-gray-800">
+          Join the Adventure
+        </h1>
+        <p className="my-2 text-lg text-gray-600 text-center">
           If you're seeking a club that combines the thrill of hiking through stunning locations with the joy of forging meaningful connections, you've found your perfect match. Join us, and let's explore the wonders of the UK together, one step at a time.
-          
           At the Imperial College London Hiking Club, we're not just a club; we're a community of nature enthusiasts, adventurers, and friends. Get ready to leave the city behind, embark on unforgettable journeys, and create memories that will last a lifetime. Welcome to the adventure of a lifetime – welcome to the Imperial College London Hiking Club!
         </p>
       </div>
